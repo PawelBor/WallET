15e9bc7a553e243f4116b2f7f28654bdb66e95c6
Pawel Borzym <pawel.borzym3095@gmail.com>
2016-11-13 16:56:01 +0000

    CRUD(create, read, update, delete (SQLite)) Operations, Balance Set/Update, Cost Calculations...

---
+++ WallET/WallET/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallET.Model
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public int productQTY { get; set; }
+        public double productPrice { get; set; }
+    }
+}
--- WallET/WallET/Views/MenuPage.xaml.cs
+++ WallET/WallET/Views/MenuPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using WallET.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,7 +17,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page product template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace WallET.Views
 {
@@ -22,9 +26,306 @@ namespace WallET.Views
     /// </summary>
     public sealed partial class MenuPage : Page
     {
+
+
+        //Set up SQLite DB + path
+        //global vars for balance, current balance and total cost.
+        private string path;
+        private SQLite.Net.SQLiteConnection conn;
+        public double balance;
+        public double currBalance;
+        double totalCost = 0;
+
         public MenuPage()
         {
+            //path for Database + Creating Table
             this.InitializeComponent();
+            this.SetBalance();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.budgetControl");
+            dbConnection();
+            conn.CreateTable<Product>();
+            closeDBconnection();
+
+            this.checkFileContent();
         }
+
+        private void SetBalance()
+        {
+            //curent userSetBalance. 
+            currBalance = balance - totalCost;
+            curentBalanceBox.Text = currBalance.ToString();
+        }
+        private async void balanceStorage()
+        {
+            //get a link to current folder
+            StorageFolder balanceFolder = ApplicationData.Current.LocalFolder;
+            //file userSetBalance
+            StorageFile userSetBalance;
+
+            try
+            {
+                //Gets file, content and puts it into string.
+                userSetBalance = await balanceFolder.GetFileAsync("balance.txt");
+            }
+            catch (Exception E)
+            {
+                //If the file does not exist, create it.
+                string message = E.Message;
+                userSetBalance = await balanceFolder.CreateFileAsync("balance.txt");
+            }
+            //Add file content
+            string output;
+            output = balance.ToString();
+            //write to file the original contents, also output of balance
+            await Windows.Storage.FileIO.WriteTextAsync(userSetBalance, output);
+            //SHow message
+            await new MessageDialog("saved to file").ShowAsync();
+        }
+
+        private async void checkFileContent()
+        {
+            //get a link to current folder and read from locations.txt file
+            StorageFolder balanceFolder = ApplicationData.Current.LocalFolder;
+
+            StorageFile userSetBalance;
+            try
+            {
+                //get the file and its contents to a string
+                userSetBalance = await balanceFolder.GetFileAsync("balance.txt");
+            }
+            catch (Exception E)
+            {
+                string message = E.Message;
+                return;
+            }
+            //read from the file
+            string fileText = await Windows.Storage.FileIO.ReadTextAsync(userSetBalance);
+            //output file contents to textblock
+            balanceBox.Text = fileText;
+
+            balance = Convert.ToDouble(fileText);
+
+
+        }
+
+        private async void btnSetBalance_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBalanceInput.Text.ToString() != "")
+            {
+                try
+                {
+                    balance = Convert.ToDouble(txtBalanceInput.Text);
+                    balanceBox.Text = balance.ToString();
+
+                    SetBalance();
+                    balanceStorage();
+                }
+                catch
+                {
+                    MessageDialog Dialog = new MessageDialog("Input must be numeric");
+                    await Dialog.ShowAsync();
+                }
+
+            }
+
+        }
+
+
+
+
+
+        //ADD
+        //create SQLite DB connection
+        //create variable "addProduct" that holds SQLite insert statement
+        //set each instance variable in the Item class with values from each TextBox
+        //Call Show_tapped func to asynchronously update page
+        //close db conn
+        //clear textboxes after adding
+        private async void Add_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            dbConnection();
+            try
+            {
+
+                if (!IsDecimal(quantityTextBox) || !IsDecimal(priceTextBox))
+                {
+                    MessageDialog decimalMsg = new MessageDialog("Quantity + Price must be a number.");
+                    await decimalMsg.ShowAsync();
+                }
+                else
+                {
+                    var addProduct = conn.Insert(new Product()
+                    {
+                        productName = productTextBox.Text,
+                        productQTY = Convert.ToInt32(quantityTextBox.Text),
+                        productPrice = Convert.ToDouble(priceTextBox.Text)
+                    });
+                }
+                
+
+              
+                
+            }
+            catch (Exception)
+            {
+                MessageDialog message = new MessageDialog("Nothing to Add.");
+                await message.ShowAsync();
+            }
+            
+            //ShowAllError_Tapped(sender, e);   for showing all data when using Add_Tapped
+            closeDBconnection();
+            //clearTextBoxes();
+            Show_Tapped(sender, e);
+            SetBalance();
+
+        } // End Add_Tapped
+
+
+
+
+        //SHOW/RETRIEVE
+        //connect to DB
+        //create a List of products and populate it with values from Item Table
+        //display all in ListBox
+        //close db connection
+        private void Show_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                dbConnection();
+                List<string> products = new List<string>();
+                var listProducts = conn.Table<Product>();
+                string result = string.Empty;
+                //double totalCost = 0;
+
+                totalCost = 0;
+                foreach (var product in listProducts)
+                {
+                    result = string.Format("{0} " + Environment.NewLine + " Qty: {1}     Price: €{2}", product.productName, product.productQTY, product.productPrice);
+                    products.Add(result);
+                    totalCost += Convert.ToDouble(product.productPrice);
+                }
+
+                if (products.Count == 0)
+                {
+
+                        listBoxTotalCost.Text = "Your List is Empty!";
+                    
+                }
+                else if (products.Count > 0)
+                {
+                    listBoxTotalCost.Text = "";
+                }
+                fetchData.DataContext = products;
+
+
+                listBoxTotalItemCount.Text = ("Product Count: " + Convert.ToString(products.Count));
+                listBoxTotalCost.Text = ("Total Cost: € " + totalCost);
+
+
+                SetBalance();
+                closeDBconnection();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        } // End SHOW ALL_Tapped
+
+
+        private void Delete_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                dbConnection();
+                var listProducts = conn.Table<Product>();
+                List<string> products = new List<string>();
+                foreach (var product in listProducts)
+                {
+                    string compareResult = string.Format("{0} " + Environment.NewLine + " Qty: {1}     Price: €{2}", product.productName, product.productQTY, product.productPrice);
+
+                    if (fetchData.SelectedValue.Equals(compareResult))
+                    {
+                        conn.Delete<Product>(product.productID);
+                    }
+                }
+                Show_Tapped(sender, e);
+                closeDBconnection();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        } //  End Function
+
+
+        private async void DeleteAll_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                dbConnection();
+                var listProducts = conn.Table<Product>();
+                List<string> products = new List<string>();
+
+                foreach (var product in listProducts)
+                {
+                    conn.Delete<Product>(product.productID);
+                }
+                Show_Tapped(sender, e);
+                closeDBconnection();
+
+                MessageDialog message = new MessageDialog("All Items Removed");
+                await message.ShowAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        // Checks to ensure that only Numeric data is allowed in Qty and Price Fields, returns false if not
+        private bool IsDecimal(TextBox textBox)
+        {
+            try
+            {
+                Convert.ToDecimal(textBox.Text);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private void fetchData_GotFocus(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Delete.IsEnabled = true;
+            DeleteAll.IsEnabled = true;
+        }
+
+        private void fetcheData_LostFocus(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Delete.IsEnabled = false;
+            DeleteAll.IsEnabled = false;
+        }
+
+        //Database conn, conn variable + path of where db i.
+        private void dbConnection()
+        {
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        }
+
+
+        // Close database connection
+        private void closeDBconnection()
+        {
+            conn.Commit();
+            conn.Dispose();
+            conn.Close();
+        }
+
+
     }
 }
